6f0099bc95363421f2207b8211cd395a46485b03
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-12-09 15:44:50 -0500

    Cleaned up + refactored `windows.js`

--- js/windows.js
+++ js/windows.js
@@ -1,271 +1,173 @@
-// Function to bring the clicked window to the front
-function bringToFront(element) {
-  let maxZIndex = Math.max(...$('.windows').map(function() {
-      return parseInt($(this).css("z-index")) || 0;
-  }).get());
-  $(element).css("z-index", maxZIndex + 1); // Set higher z-index for the clicked window
-}
+$(document).ready(function() {
+  // Center windows
+  $(".mac-terminal, .text-edit, .email, .calc").position({
+    my: "center",
+    at: "center",
+    collision: "fit",
+    of: "#main-content"
+  });
 
-// Make windows draggable and bring to front on drag
-function makeDraggable(selector) {
-  $(selector).draggable({
+  // Show terminal on load
+  $('.mac-terminal').fadeIn(500); // .show(500);
+
+  // Function to bring the clicked window to the front
+  function bringToFront(element) {
+    let maxZIndex = Math.max(...$('.windows').map(function() {
+      return parseInt($(this).css("z-index")) || 0;
+    }).get());
+    // Set higher z-index for the clicked window
+    $(element).css("z-index", maxZIndex + 1);
+  }
+
+  // Make windows draggable and bring to front on drag
+  function makeDraggable(selector) {
+    $(selector).draggable({
+      handle: ".header, .notes-header, .mail-header, .calc-header", //, #spotlight_wrapper",
+      cancel: ".header__op, .notes-header__op, .mail-header__op, .calc-header__op, .send-btn",
       start: function(event, ui) {
-          bringToFront(this);
+        bringToFront(this);
       },
       containment: "#main-content",
-      distance: 0
-  }).on('click', function() {
+      distance: 0,
+      // stack: ".windows",
+    }).on('click', function() {
       bringToFront(this);
+    });
+  }
+
+  // Apply draggable to all existing windows
+  makeDraggable('.windows');
+
+  // Make folder and file icons draggable
+  $(".btn").draggable({
+    cursor: "default",
+    cancel: false,
+    containment: "#main-content",
+    distance: 0,
   });
-}
 
-$(function() {
-  $(".mac-terminal")
-    .draggable({
-      handle: "div.header",
-      cancel: "div.header__op",
-      //stack: ".windows",
-      containment: "#main-content",
-      distance: 0,
-      start: function(event, ui) {
-        bringToFront(this);
-        makeDraggable(this);
-    },
+  // $("#spotlight_wrapper")        
+  // .draggable({
+  //   cursor: "default",
+  //   cancel: false,
+  //   //stack: ".windows",
+  //   // containment: "#main-content",
+  //   // distance: 0,
+  // });
+  
+  // Open terminal
+  $('#iterm').click(function(){
+    $('.mac-terminal').css("display", "inline-block");
+    bringToFront('.mac-terminal');
+  });
+
+  // Open mail
+  $('#mail').click(function(){
+    $('.email').css("display", "flex");
+    bringToFront('.email');
+  });    
+
+  // Open about me
+  $('#notes').click(function(){
+    $('.text-edit').css("display", "flex");
+    bringToFront('.text-edit');
+  });
+
+  // Open calculator
+  $('#calculator').click(function(){
+    $('.calc').css("display", "inline-block");
+    bringToFront('.calc');
+  });
+
+  // Open trash dialogue
+  $('#trash-icon').click(function(){
+    $('.dialogue').css("display", "inline-block");
+    bringToFront('.dialogue');
+  });
+
+  // Empty trash
+  $(".confirm").click(function (e) {
+    e.preventDefault();
+    $('#trash').attr('src', 'assets/icons/empty_trash.png');
+    $("#trash-icon").off("click");
+  });
+
+  // Close window
+  function closeWindow(close, win) {
+    $(close).on('click', function() {
+      $(win).css("display", "none");
     });
+  }
+  
+  // Close terminal
+  closeWindow('.header__op-icon--red', '.mac-terminal');
 
-  $(".text-edit")
-    .draggable({
-      handle: "div.notes-header",
-      cancel: "div.notes-header__op",
-      //stack: ".windows",
-      containment: "#main-content",
-      distance: 0,
-      start: function(event, ui) {
-        bringToFront(this);
-        makeDraggable(this);
-    },
-    });
+  // Close mail
+  closeWindow('.mail-header__op-icon--red', '.email');
 
-    $(".email")
-    .draggable({
-      handle: "div.mail-header",
-      cancel: "div.mail-header__op, .send-btn",
-      //stack: ".windows",
-      containment: "#main-content",
-      distance: 0,
-      start: function(event, ui) {
-        bringToFront(this);
-        makeDraggable(this);
-    },
-    });
+  // Close about me
+  closeWindow('.notes-header__op-icon--red', '.text-edit');
 
-  $(".calc")
-    .draggable({
-      handle: "div.calc-header",
-      cancel: "div.calc-header__op",
-      //stack: ".windows",
-      containment: "#main-content",
-      distance: 0,
-      start: function(event, ui) {
-        bringToFront(this);
-        makeDraggable(this);
-    },
-    });
+  // Close calculator
+  closeWindow('.calc-header__op-icon--red', '.calc');
 
-  $(".btn")        
-    .draggable({
-      cursor: "default",
-      cancel: false,
-      containment: "#main-content",
-      distance: 0,
-    });
-
-    // $("#spotlight_wrapper")        
-    // .draggable({
-    //   cursor: "default",
-    //   cancel: false,
-    //   //stack: ".windows",
-    //   // containment: "#main-content",
-    //   // distance: 0,
-    //   start: function(event, ui) {
-    //     bringToFront(this);
-    //     makeDraggable(this);
-    // },
-    // });
-
-    $(".dialogue")        
-    .draggable({
-      cursor: "default",
-      cancel: ".alert-btn",
-      //stack: ".windows",
-      containment: "#main-content",
-      distance: 0,
-      start: function(event, ui) {
-        bringToFront(this);
-        makeDraggable(this);
-    },
-    });
-});
-
-$(document).ready(function() {
-    // Center windows
-    $(".mac-terminal, .text-edit, .email, .calc").position({
-      my: "center",
-      at: "center",
-      collision: "fit",
-      of: "#main-content"
-    });
-
-    // Show terminal on load
-    $('.mac-terminal').fadeIn(500); // .show(500);
-
-    // Function to bring the clicked window to the front
-    function bringToFront(element) {
-        let maxZIndex = Math.max(...$('.windows').map(function() {
-            return parseInt($(this).css("z-index")) || 0;
-        }).get());
-        $(element).css("z-index", maxZIndex + 1); // Set higher z-index for the clicked window
-    }
-
-    // Make windows draggable and bring to front on drag
-    function makeDraggable(selector) {
-        $(selector).draggable({
-            handle: ".header, .notes-header, .mail-header, .calc-header", //, #spotlight_wrapper",
-            cancel: ".header__op, .notes-header__op, .mail-header__op, .calc-header__op, .send-btn",
-            start: function(event, ui) {
-                bringToFront(this);
-            },
-            containment: "#main-content",
-            distance: 0
-        }).on('click', function() {
-            bringToFront(this);
-        });
-    }
-
-    // Apply draggable to all existing windows
-    makeDraggable('.windows');
-
-    // Open terminal
-    $('#iterm').click(function(){
-      $('.mac-terminal').css("display", "inline-block").addClass('windows');
-      bringToFront('.mac-terminal');
-      makeDraggable('.mac-terminal');
-    });
-
-    // Open mail
-    $('#mail').click(function(){
-      $('.email').css("display", "flex").addClass('windows');
-      bringToFront('.email');
-      makeDraggable('.email');
-    });    
-
-    // Open about me
-    $('#notes').click(function(){
-      $('.text-edit').css("display", "flex").addClass('windows');
-      bringToFront('.text-edit');
-      makeDraggable('.text-edit');
-    });
-
-    // Open calculator
-    $('#calculator').click(function(){
-      $('.calc').css("display", "inline-block").addClass('windows');
-      bringToFront('.calc');
-      makeDraggable('.calc');
-    });
-
-    // Open trash dialogue
-    $('#trash-icon').click(function(){
-      $('.dialogue').css("display", "inline-block").addClass('windows');
-      bringToFront('.dialogue');
-      makeDraggable('.dialogue');
-    });
-
-    // Close trash dialogue
-    $('.alert-btn').click(function(){
-      $('.dialogue').css("display", "none");
-    });
-
-    // Close terminal
-    $('.header__op-icon--red').click(function(){
-      $('.mac-terminal').css("display", "none");
-    });
-
-    // Close mail
-    $('.mail-header__op-icon--red').click(function(){
-      $('.email').css("display", "none");
-    });  
-
-    // Close about me
-    $('.notes-header__op-icon--red').click(function(){
-      $('.text-edit').css("display", "none");
-    });
-
-    // Close calculator
-    $('.calc-header__op-icon--red').click(function(){
-      $('.calc').css("display", "none");
-    });
-
-    // Empty trash
-    $(".confirm").click(function (e) {
-      e.preventDefault();
-      $('#trash').attr('src', 'assets/icons/empty_trash.png');
-      $("#trash-icon").off("click");
-    });
+  // Close trash dialogue
+  closeWindow('.alert-btn', '.dialogue');  
     
-    // Minimize terminal
-    $('.header__op-icon--yellow').click(function(){
-        // $('.mac-terminal').css("transform", "translateY(82%) translateX(0%) scale(0.75)");
-        // $('.mac-terminal').css("transition", "all 0.25s");
-      $('.mac-terminal').toggleClass('minimize');
-      $('.mac-terminal').removeClass('maximize');
-    });
-
-    // Maximize terminal
-    $('.header__op-icon--green').click(function(){
-      $('.mac-terminal').toggleClass('maximize');
-      $('.mac-terminal').removeClass('minimize');
-    });
-
-    $('#bold-btn').click(function(){
-      $('.text-body').toggleClass("bold");
-    });
-
-   $('#italic-btn').click(function(){
-      $('.text-body').toggleClass("italic");
-   });
-
-   $('#underline-btn').click(function(){
-      $('.text-body').toggleClass("underline");
-   });
-
-   $('#left-btn').click(function(){
-      $('.text-body').toggleClass("left");
-      $('.text-body').removeClass("right");
-      $('.text-body').removeClass("center");
-      $('.text-body').removeClass("justify");
-   });
-
-   $('#center-btn').click(function(){
-      $('.text-body').toggleClass("center");
-      $('.text-body').removeClass("right");
-      $('.text-body').removeClass("left");
-      $('.text-body').removeClass("justify");
-   });
-
-   $('#right-btn').click(function(){
-      $('.text-body').toggleClass("right");
-      $('.text-body').removeClass("left");
-      $('.text-body').removeClass("center");
-      $('.text-body').removeClass("justify");
-   });
-
-   $('#justify-btn').click(function(){
-      $('.text-body').toggleClass("justify");
-      $('.text-body').removeClass("right");
-      $('.text-body').removeClass("center");
-      $('.text-body').removeClass("left");
-   });
+  // Minimize terminal
+  $('.header__op-icon--yellow').click(function(){
+      // $('.mac-terminal').css("transform", "translateY(82%) translateX(0%) scale(0.75)");
+      // $('.mac-terminal').css("transition", "all 0.25s");
+    $('.mac-terminal').toggleClass('minimize');
+    $('.mac-terminal').removeClass('maximize');
   });
+
+  // Maximize terminal
+  $('.header__op-icon--green').click(function(){
+    $('.mac-terminal').toggleClass('maximize');
+    $('.mac-terminal').removeClass('minimize');
+  });
+
+  $('#bold-btn').click(function(){
+    $('.text-body').toggleClass("bold");
+  });
+
+  $('#italic-btn').click(function(){
+    $('.text-body').toggleClass("italic");
+  });
+
+  $('#underline-btn').click(function(){
+    $('.text-body').toggleClass("underline");
+  });
+
+  $('#left-btn').click(function(){
+    $('.text-body').toggleClass("left");
+    $('.text-body').removeClass("right");
+    $('.text-body').removeClass("center");
+    $('.text-body').removeClass("justify");
+  });
+
+  $('#center-btn').click(function(){
+    $('.text-body').toggleClass("center");
+    $('.text-body').removeClass("right");
+    $('.text-body').removeClass("left");
+    $('.text-body').removeClass("justify");
+  });
+
+  $('#right-btn').click(function(){
+    $('.text-body').toggleClass("right");
+    $('.text-body').removeClass("left");
+    $('.text-body').removeClass("center");
+    $('.text-body').removeClass("justify");
+  });
+
+  $('#justify-btn').click(function(){
+    $('.text-body').toggleClass("justify");
+    $('.text-body').removeClass("right");
+    $('.text-body').removeClass("center");
+    $('.text-body').removeClass("left");
+  });
+});
 
 const colorPicker = document.getElementById("colorPicker");
 colorPicker.addEventListener("input", function() {
@@ -281,11 +183,3 @@ const fontFamily = document.getElementById("fontFamily");
 fontFamily.addEventListener("change", function() {
   $('.text-body').css("font-family", this.value);
 });
-
-// var closeWindow = function() {
-//   $('.window-close').bind('click', function(e) {
-//     e.preventDefault();
-//     $(this).parents('.windows').removeClass('current-window window-opened').addClass('window-closed').hide();
-//   });
-// };
-// closeWindow();
