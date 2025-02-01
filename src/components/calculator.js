6859af5b5a232916a9392bb737d2f9dffdb0a471
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-02-01 18:43:24 -0500

    Remove comments from `calculator.js`

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,28 +1,21 @@
 $(".val").click(function(e) {
-  // prevent the link from acting like a link
   e.preventDefault();
 
-  //grab this link's href value
   var a = $(this).attr("href");
-
-  // append said value to the screen
   var screenVal;
+
   if (((a == "*-1") || (a == "%") || (a == "*") || (a == "/") || (a == "-") || (a == "+")) && ($(".outcome").val() == "")) return;
   else if (a == "*") screenVal = "×";
   else if (a == "/") screenVal = "÷";
   else if (a == "-") screenVal = "−";
   else screenVal = a;
-  $(".screen").append(screenVal);
 
-  // append same value to a hidden input
+  $(".screen").append(screenVal);
   $(".outcome").val($(".outcome").val() + a);
 });
 
 $(".equal").click(function() {
-  // solve equation and put in hidden field
   $(".outcome").val(eval($(".outcome").val()));
-
-  // take hidden field's value & put it on screen
   $(".screen").html(eval($(".outcome").val()));
 });
 
