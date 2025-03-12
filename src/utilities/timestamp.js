068324b6c55d4ade0afba5cf3cdc1a32dca0b65f
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-03-11 21:20:12 -0400

    Add `"DOMContentLoaded"` event listener to `timestamp.js`

--- src/utilities/timestamp.js
+++ src/utilities/timestamp.js
@@ -1,31 +1,33 @@
-const days = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
-const months = [ "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec" ];
+document.addEventListener("DOMContentLoaded", function() {
+  const days = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+  const months = [ "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec" ];
 
-function initTimestamp() {
-  const date = new Date();
-  const weekDay = days[date.getDay()];
-  const day = date.getDate();
-  const month = months[date.getMonth()];
-  const time = date.toLocaleTimeString("en-us", { hour12: false });
-  return weekDay.concat(" ", month, " ", day, " ", time);
-}
+  function initTimestamp() {
+    const date = new Date();
+    const weekDay = days[date.getDay()];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const time = date.toLocaleTimeString("en-us", { hour12: false });
+    return weekDay.concat(" ", month, " ", day, " ", time);
+  }
 
-function currentTimestamp() {
-  const date = new Date();
-  return date.toLocaleTimeString("en-us", { hour12: false });
-}
+  function currentTimestamp() {
+    const date = new Date();
+    return date.toLocaleTimeString("en-us", { hour12: false });
+  }
 
-function updateTimestamp() {
-  document.querySelector("#menutime").textContent = currentTimestamp();
-}
+  function updateTimestamp() {
+    document.querySelector("#menutime").textContent = currentTimestamp();
+  }
 
-setInterval(updateTimestamp, 1000);
-updateTimestamp();
+  setInterval(updateTimestamp, 1000);
+  updateTimestamp();
 
-function randomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // Min and max are inclusive
-}
+  function randomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // Min and max are inclusive
+  }
 
-document.getElementById("timestamp").innerHTML = "Last login: ".concat(initTimestamp(), " on ttys00", randomInt(0, 6));
+  document.getElementById("timestamp").innerHTML = "Last login: ".concat(initTimestamp(), " on ttys00", randomInt(0, 6));
+});
