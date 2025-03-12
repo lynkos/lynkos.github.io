f097887138bf8d1548bbceb31547148198639e8f
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-03-11 21:17:11 -0400

    Rewrite `notes.js` without jQuery (i.e. jQuery --> JavaScript)

--- src/components/notes.js
+++ src/components/notes.js
@@ -1,26 +1,31 @@
-$(function() {
+document.addEventListener("DOMContentLoaded", function() {
     // Highlight clicked nav item
-    $(".child-nav li", ".sidebar").on("click", function() {
-        $(".child-nav li", ".sidebar").removeClass("active");
-        if (!$(this).hasClass("active")) $(this).addClass("active");
+    document.querySelectorAll(".sidebar .child-nav li").forEach(function(element) {
+        element.addEventListener("click", function() {
+            document.querySelectorAll(".child-nav li").forEach(function(element) {
+                element.classList.remove("active");
+            });
+            
+            document.querySelector(".sidebar").classList.remove("active");
+            if (!this.classList.contains("active")) this.classList.add("active");
+        });
     });
-});
 
-// Make selected project in Notes sidebar active and all others inactive
-var selectProject = function(element) {
-    var projectInfo = document.getElementsByClassName("project");
+    // Make selected project in Notes sidebar active and all others inactive
+    var selectProject = function(element) {
+        var projectInfo = document.getElementsByClassName("project");
 
-    for (let j = 0; j < projectInfo.length; ++j) {
-        projectInfo[j].classList.remove("active");
+        for (let j = 0; j < projectInfo.length; ++j)
+            projectInfo[j].classList.remove("active");
+
+        var project = document.querySelector("#" + element.dataset.id);
+        project.classList.add("active");
     }
 
-    var project = document.querySelector("#" + element.dataset.id);
-    project.classList.add("active");
-}
-
-// Select project from projects in Notes sidebar
-document.querySelectorAll(".project-name").forEach(function(element) {
-    element.addEventListener("click", function(event) {
-        selectProject(event.target)
+    // Select project from projects in Notes sidebar
+    document.querySelectorAll(".project-name").forEach(function(element) {
+        element.addEventListener("click", function(event) {
+            selectProject(event.target)
+        });
     });
 });
