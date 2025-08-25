a4f55bbe1b3b36209ccf21806e9ff82ae8ddc696
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-08-24 23:09:58 -0400

    Rewrite without Vue, add customized version of 'Not by AI' badge (to match style)

--- src/components/404.js
+++ src/components/404.js
@@ -133,31 +133,3 @@ function setupEvents() {
         pointer.tY = eY;
     }
 }
-
-var app = new Vue({
-    el: "#mount",
-    data() {
-    return {
-        wdo: {},
-        title: "Arrow Glaze",
-        aero: {
-        basic: false,
-        transparency: CSS.supports('color', 'rgba(0,0,0,0.5)'),
-        blur: CSS.supports('backdrop-filter', 'blur(1px)'),
-        css: {
-            boxShadow: true,
-            overflowHidden: true,
-            dropShadowFilter: false
-        }
-        },
-        showSidebar: false
-    }},
-    beforeDestroy() {
-    this.wdo.destroy();
-    }
-});
-
-$(".window").draggable({
-    handle: ".titlebar",
-    containment: "#mount"
-});
